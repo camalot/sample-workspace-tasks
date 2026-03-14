66859c8f7ed1456da823f36a1cb038464c532ec0
Ryan Conrad <camalot@gmail.com>
2026-03-14 16:42:49 -0500

    update some test scripts

---
+++ apps/appB/script1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "This is the script1.sh script in apps/appB"
+sleep 5
+echo "This is the end of the script1.sh script in apps/appB"
---
+++ apps/appC/script1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "This is the script1.sh script in apps/appC"
+sleep 5
+echo "This is the end of the script1.sh script in apps/appC"
